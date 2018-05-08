be04e36e728159771b0068868d832ff7eff7661e
Jon Kuperman <jon.kuperman@gmail.com>
2018-05-07 21:59:12 -0700

    eslint config and jsx a11y

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
-import React, { Component } from "react";
-import "./App.css";
+import React from "react";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div>
+    <h1>A Basic Website</h1>
+    <div>🐼 are cute</div>
+    <img src="cat.jpg" />
+  </div>
+);
 
 export default App;
